cbbbbdc59a7fa38b31f93d43c074a9ef0f680e34
usmanwalana <usman@walana.org>
2016-11-15 18:30:48 +0500

    Adjusted code to make Plugin work
    Plugin is now loading static data but unable to load data from web
    service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,44 @@
-import { Component } from '@angular/core';
-import {AppService} from "./app.service";
-
+import { Component , OnInit} from '@angular/core';
+import {Select2TemplateFunction , Select2OptionData} from 'ng2-select2/ng2-select2';
+import {Http} from '@angular/http';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent
+export class AppComponent implements OnInit
 {
     private startValue:string = 'car3';
     private selected:string = "";
-
-    constructor(public data:AppService)
+    my : any[];
+    test: Select2OptionData[] = [];
+    constructor(private http : Http)
     {
     }
+
+    getUsers()
+    {
+        return this.http.get("https://jsonplaceholder.typicode.com/users");
+    }
+    ngOnInit()
+    {
+        this.getUsers().subscribe(
+            success => {
+                this.my = success.json();
+                this.populateUsers();
+            }
+        )
+    }
+    populateUsers()
+    {
+        for(let user of this.my)
+        {
+            this.test.push({id : user.id , text : user.name});
+
+        }
+        console.log(this.test);
+    }
+
 
     // function for result template
     public templateResult:Select2TemplateFunction = (state:Select2OptionData):JQuery | string =>
@@ -44,7 +69,7 @@ export class AppComponent
         return jQuery('<span><b>' + state.additional.winner + '.</b> ' + state.text + '</span>');
     }
 
-    public changed(e:Object):void
+    public changed(e:any):void
     {
         this.selected = e.value;
     }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {AppService} from "./app.service";
@@ -13,6 +14,7 @@ import { Select2Component } from 'ng2-select2/ng2-select2';
   ],
   imports: [
     BrowserModule,
+      HttpModule,
   ],
   providers: [
     AppService],
